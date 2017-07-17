7c0e5633bafaf45d232990bd076430a73172d042
Sam Wei <samw2k00@gmail.com>
2017-07-17 12:42:04 +1000

    Working PanResponder

--- CollapsibleHeaderWithPanResponder.js
+++ CollapsibleHeaderWithPanResponder.js
@@ -1,8 +1,6 @@
 /*
-  This code is heavily modified to suited the client need,
-  Original  https://github.com/sonaye/react-native-collapsible-header
-  By https://github.com/sonaye
-  Modify by Sam Wei
+  Collapsible using panResponder
+  Created by Sam Wei
 */
 import React, { Component } from 'react'
 import { Animated, FlatList, View, PanResponder } from 'react-native'
@@ -18,103 +16,57 @@ export default class CollapsibleHeader extends Component {
     headerHeight: PropTypes.any.isRequired,
     data: PropTypes.any
   }
-  scrollAnim = new Animated.Value(0);
-  offsetAnim = new Animated.Value(0);
+  scroll = new Animated.Value(0);
 
-  clampedScrollValue = 0;
-  offsetValue = 0;
-  scrollValue = 0
 
-  state = {
-    scrollAnim: this.scrollAnim,
-    offsetAnim: this.offsetAnim,
-    clampedScroll: Animated.diffClamp(
-      Animated.add(
-        this.scrollAnim.interpolate({
-          inputRange: [0, 1],
-          outputRange: [1, 0],
-          extrapolateRight: 'clamp'
-        }),
-        this.offsetAnim
-      ),
-      0,
-      this.props.headerHeight //label height
-    )
-  };
+  position = this.scroll.interpolate({
+    inputRange: [-(this.props.headerHeight), 0, this.props.headerHeight],
+    outputRange: [-(this.props.headerHeight), 0, 0],
+    extrapolate: 'clamp'
+  });
+
+  opacity = this.scroll.interpolate({
+    inputRange: [-(this.props.headerHeight), 0, this.props.headerHeight],
+    outputRange: [0, 1, 1]
+  });
+
+  componentWillUnmount() {
+    this.scroll.removeAllListeners()
+  }
 
   componentWillMount() {
-
+    this._animatedValueY = 0
+    this.scroll.addListener((value) => {
+      if (value.value < 0) {
+        if (value.value < -Math.abs(this.props.headerHeight)) {
+          this._animatedValueY = -Math.abs(this.props.headerHeight)
+        } else {
+          this._animatedValueY = Math.ceil(value.value)
+        }
+      }
+      if (value.value > 0) {
+        if (value.value > this.props.headerHeight) {
+          this._animatedValueY = this.props.headerHeight
+        } else {
+          this._animatedValueY = Math.ceil(value.value)
+        }
+      }
+    })
     this.scollerPanResponder = PanResponder.create({
       onStartShouldSetPanResponder: (evt, gestureState) => true,  //eslint-disable-line 
       onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: (e, gestureState) => { //eslint-disable-line 
+        this.scroll.setOffset(this._animatedValueY)
+        this.scroll.setValue(0) //Initial value
+      },
       onPanResponderMove: Animated.event([
-        null, { dy: this.state.scrollAnim }
-      ]),
-      onPanResponderRelease: (evt, gestureState) => {
-        console.log("dy", gestureState.dy)
-
-        const toValue = this.scrollValue > this.props.headerHeight &&
-          this.clampedScrollValue > (this.props.headerHeight) / 2
-          ? this.offsetValue - this.props.headerHeight
-          : this.offsetValue + this.props.headerHeight
-
-        Animated.timing(this.state.offsetAnim, {
-          toValue,
-          duration: 0,
-          useNativeDriver: false
-        }).start()
-        // if (gestureState.dy < 0) {
-        //   this.offsetAnim.setValue(this.props.headerHeight)
-        // }else {
-        //   this.offsetAnim.setValue(-(this.props.headerHeight))
-        // }
-      }
+        null, { dy: this.scroll }
+      ])
     })
   }
-
-  componentDidMount() {
-    this.state.scrollAnim.addListener(({ value }) => {
-      console.log("---ScrollAnim Value:", value)
-      const diff = value - this.scrollValue
-      this.scrollValue = value
-      this.clampedScrollValue = Math.min(
-        Math.max(this.clampedScrollValue + diff, 0),
-        this.props.headerHeight * 2
-      )
-    })
-
-    this.state.offsetAnim.addListener(({ value }) => {
-      console.log("+++ OffsetAnim valye:", value)
-      this.offsetValue = value
-    })
-
-  }
-
-  componentWillUnmount() {
-    this.state.scrollAnim.removeAllListeners()
-    this.state.offsetAnim.removeAllListeners()
-  }
-
-  onScrollEndDrag = () => {
-    console.log("OnScroll end", this.state.offsetAnim._value)
-    console.log("on scrollvalue", this.scrollValue)
-  };
 
 
   render() {
-    const { clampedScroll } = this.state
-
-    const navbarTranslate = clampedScroll.interpolate({
-      inputRange: [0, this.props.headerHeight],
-      outputRange: [0, -(this.props.headerHeight)],
-      extrapolate: 'clamp'
-    })
-
-    const navbarOpacity = clampedScroll.interpolate({
-      inputRange: [0, this.props.headerHeight],
-      outputRange: [1, 0],
-      extrapolate: 'clamp'
-    })
 
     return (
       <View style={{ flex: 1 }}>
@@ -123,17 +75,17 @@ export default class CollapsibleHeader extends Component {
             {
               backgroundColor: this.props.headerColor || '#fff',
               height: this.props.headerHeight,
-              transform: [{ translateY: navbarTranslate }]
+              transform: [{ translateY: this.position }]
             }
           ]}>
-          <Animated.View style={{ flex: 1, opacity: navbarOpacity }}>
+          <Animated.View style={{ flex: 1, opacity: this.opacity }}>
             {this.props.renderHeader}
           </Animated.View>
         </Animated.View>
         <Animated.View
           style={[
             {
-              transform: [{ translateY: navbarTranslate }]
+              transform: [{ translateY: this.position }]
             }
           ]}>
           <FlatList
@@ -150,4 +102,3 @@ export default class CollapsibleHeader extends Component {
     )
   }
 }
-
